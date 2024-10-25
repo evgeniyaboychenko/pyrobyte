f0d95a6fa7ec0a587cf069978767b038c431479e
evgeniyaboychenko <evgeniyaboychenko@gmail.com>
2024-10-25 14:34:48 +0700

    fix bugs

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/pyrobyte",
+  base: "/pyrobyte/",
   css: {
     preprocessorOptions: {
       scss: {
         api: 'modern-compiler',
       }
     }
-  }
+  },
 })
